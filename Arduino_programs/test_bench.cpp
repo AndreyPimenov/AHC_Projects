f3ca61f86452143da2c1459af4c5ce77b93f0e43
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2021-02-24 19:17:05 +0300

    Update test_bench.cpp

--- Arduino_programs/test_bench.cpp
+++ Arduino_programs/test_bench.cpp
@@ -13,6 +13,18 @@ Pneumatic pa_second(7); // << --------- RESERVE
 
 // Block of the variables:
 int point = 0;
+bool state_flag = 0;
+unsigned long start_time; 
+
+
+// pause_function:
+void milsec_pause(int delta){
+  int timing;
+  timing = millis();
+  while (millis() < (timing + delta) ){  
+  //Serial.println ("10 seconds");
+ };
+  }
 
 void setup() {
 
@@ -33,7 +45,7 @@ void loop() {
 //stepper1.setSpeed(600);
 //stepper1.runSpeed();
 
-point = stepper1.currentPosition() + 1600 ; // << ---- CHANGE DISTANCE HERE. where 1600 steps
+point = stepper1.currentPosition() + 200 ; // << ---- CHANGE DISTANCE HERE. where 1600 steps
 stepper1.moveTo(point);
 
 while (stepper1.distanceToGo() != 0){
@@ -43,7 +55,18 @@ while (stepper1.distanceToGo() != 0){
 delay(500); // << --- it saves from unlimited movement
 
 // C H E C K I N G _ T H E _ P N E U M A T I C _ S Y S T E M _
+
 if (stepper1.currentPosition() == point){
-  pa_first.on();  delay (1000);    pa_first.off(); delay(1000);
+
+  if (state_flag == 1){
+     pa_first.on();  //delay (1000);
+     //start_time = millis()
+     milsec_pause(1000); 
+    }
+  else if (state_flag == 0){ 
+    pa_first.off();  //delay (1000); 
+    milsec_pause(1000);
+    }
+  state_flag = !state_flag; 
   }
 }
