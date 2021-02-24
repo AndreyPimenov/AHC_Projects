8500208309b83df767810153fe299d232e162858
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2021-02-24 12:42:02 +0300

    Update test_bench.cpp

--- Arduino_programs/test_bench.cpp
+++ Arduino_programs/test_bench.cpp
@@ -1,9 +1,5 @@
-# for AHC Revolution
 #include <AccelStepper.h>
-#include "Button.h"
 #include "Pneumatic.h"
-
-// Think about using liblary PinChangeInterrrupt
 
 // main driver
 #define step_main 10
@@ -12,33 +8,42 @@
 // Create objects:
 AccelStepper stepper1(AccelStepper::DRIVER, step_main, dir_main);
 
-btn start ... 
-btn stop ....
-
-pa_act1 ... 
-pa_act2 .... 
+Pneumatic pa_first(8);
+Pneumatic pa_second(7); // << --------- RESERVE
 
 // Block of the variables:
-int freq = 4000;
-volatile bool stop_flg = false; //
-volatile bool recharge_flg = false;
-volatile bool start_flg = false;
-volatile uint32_t debounce;
-int end_time = 0;
-int act_time = 0;
+int point = 0;
 
-void loop(){
+void setup() {
+
+  pinMode(step_main, OUTPUT);
+  pinMode(dir_main, OUTPUT);
+  digitalWrite(dir_main, HIGH);
+
+  stepper1.setMaxSpeed(6000.0);    //
+  stepper1.setAcceleration(2000.0); //
   
-  act_time = millis();
-    if ((act_time - ent_time) > 2000){ 
-      // ---------------------------- time delay
-      stepper2.setSpeed(-1500);
-      stepper2.runSpeed();
-      }
-  // Think how to organise sequency generator on Arduino. 
-  
-  // можно использовать блок который я использовал для сервомотрчика во время работы шагового двигателя
-  
+  // F O R _ D E B U G G I N G:
+  // Serial.begin(9600);
 }
 
+void loop() {
 
+// C H E C K I N G _ T H E _ S T E P P E R _ S Y S T E M 
+//stepper1.setSpeed(600);
+//stepper1.runSpeed();
+
+point = stepper1.currentPosition() + 1600 ; // << ---- CHANGE DISTANCE HERE. where 1000 steps
+stepper1.moveTo(point);
+
+while (stepper1.distanceToGo() != 0){
+  stepper1.run();
+  }
+
+delay(500); // << --- it saves from unlimited movement
+
+// C H E C K I N G _ T H E _ P N E U M A T I C _ S Y S T E M _
+if (stepper1.currentPosition() == point){
+  pa_first.on();  delay (1000);    pa_first.off(); delay(1000);
+  }
+}
