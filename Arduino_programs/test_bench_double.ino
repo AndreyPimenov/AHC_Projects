64c1256a72bfabb542e9241fe1d9bdd6b0720e07
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2021-03-11 16:05:41 +0300

    Update test_bench_double.ino

--- Arduino_programs/test_bench_double.ino
+++ Arduino_programs/test_bench_double.ino
@@ -14,76 +14,41 @@ Pneumatic pa_first(8);
 
 // -------------------------- Block of variables:
 int point = 0;
-bool state_flag = 0;
-unsigned long start_time; 
+unsigned long start_time = 0; 
 
 // -------------------------- Block of functions:
-void milsec_pause(int delta){
-  int timing;
-  timing = millis();
-  while (millis() < (timing + delta) ){  };
-}
-
-void rotation_function(int steps, bool dir_flag, int half_frequency, int time_delay){
-/* 
- *  steps - кол-во шагов, например для 16-кратного мы использовали 140 импульсов
- *  dir_flag - отвечает за направление вращения (меняй: 1 или 0 как тебе нужно)
- *  Half_frequency - несколько милисекунд на то чтобы физически двигатель повернулся, думаю хватит значений 1 - 5 (милисекунд) 
- *  time_delay - задает паузу после перемещения, думаю от 100 до 300 более чем достаточно
- *  Базовое значение функции: rotation_function(140, 1, 2, 100);
-
-*/
-digitalWrite(dir_main, dir_flag); 
-
-for(int j = 0; j < steps; j++) {
-digitalWrite(step_main, HIGH);
-milsec_pause(half_frequency);                 
-digitalWrite(step_main, LOW);
-milsec_pause(half_frequency);
-}
-
-milsec_pause(time_delay);      
-}
-
-
 void setup() {
-
- stepper1.setMaxSpeed(-6400.0);
+ stepper1.setSpeed(-6400.0); // тут вместо setMaxSpeed() (- по идее обороты в минуту, но врет) используй setSpeed() - импульсы в секунду.
  stepper1.setAcceleration(-6400.0);
  stepper1.moveTo(-1000000);
     
- stepper2.setMaxSpeed(6400.0);
+ stepper2.setSpeed(6400.0);
  stepper2.setAcceleration(6400.0);
  stepper2.moveTo(1000000);
-
- 
-  
 }
 
 void loop() {
 
-delay (100);
+stepper1.runSpeed(); // c заданной скоростью без ускорения (по идее setAccelaration() теряяет смысл в этом случае
+stepper2.runSpeed();
 
-point = stepper2.currentPosition() + 140 ; // << ---- CHANGE DISTANCE HERE. where 1600 steps is half. 140 for gofr
-stepper2.moveTo(point);
+// тут добавляем прерывание по кнопке и внутри него присваиваем start_time = 0; 
+// для UNO: INT 0 - D2, INT 1 - D3
+// см. как это сделано в клипсомете:
 
-while (stepper2.currentPosition() != point) { 
-  stepper1.run();
-  stepper2.run();
+
+
+// Период срабатывания реле 500мс:
+if ((millis() - start_time) > 500){
+  start_time = millis(); 
+  if (pa_first.state_return() == true){
+    pa_first.off(); 
+    //delay(200);
   }
-
-delay (100);
-
-
-if (pa_first.state_return() == true){
-  pa_first.off(); 
-  delay(500);
+  else{
+    pa_first.on();
+    //delay(200);
+  }
 }
 
-else{
-  pa_first.on();
-  delay(500);
-  }
-delay(200);
 
-}
