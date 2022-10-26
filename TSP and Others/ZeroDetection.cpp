fe466aac7cc575f0ed1ba94dc85f2113b5acbc95
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2022-10-26 21:31:34 +0300

    Update ZeroDetection.cpp

--- TSP and Others/ZeroDetection.cpp
+++ TSP and Others/ZeroDetection.cpp
@@ -1,10 +1,13 @@
 // Timer 0 Channel 1 = pin 5, Channel B = pin 6 
 #include "GyverTimers.h"
+#include "GyverTimer.h"
+
+GTimer myTimer(US, 5000); // таймер задержки на включение
+GTimer meandr(US, 5000); //  время длина меандра
 
 // переменные, которые изменяют свое значение в прерывании - volatile
-volatile int counter = 0; // счетчик прерываний
 volatile bool signalStart = false;
-volatile bool flag = false;
+volatile bool control_flag = false;
 uint32_t debounce;
 int frequency = 25;
 
@@ -12,8 +15,8 @@ bool digitalReadFast(uint8_t pin); // быстрый опрос пина
 
 void setup(){
   pinMode(5, OUTPUT);
-  Timer0.setFrequency(frequency * 2);                  // настроить частоту таймера в Гц (умножаем на 2 из-за особенностей генерации меандра
- 
+  myTimer.setInterval(5000); // 1мс
+  meandr.setInterval(5000);  // 4мс
   Serial.begin(250000);   // 9600 115200 250000
   pinMode(3, INPUT);
   attachInterrupt(1, function, CHANGE); // LOW, FALLING - срез, RISING - фронт, CHANGE - изменение
@@ -32,25 +35,35 @@ bool digitalReadFast(uint8_t pin) {
 void function(){ // CHANGE не предоставляет состояния пина,придется узнать его при помощи digitalReadFast
   if (micros() - debounce >= 40 && digitalReadFast(3)){
      debounce = micros();
-     flag = true;
      signalStart = true;  // выставляем флаг
+     //digitalWrite(5, HIGH);
   }
 }
 
-
-
-
 void loop(){
 
-if (signalStart == true){ 
-    Timer0.start outputEnable(CHANNEL_B, HIGH);  
+
+if ((signalStart == true)&&(!control_flag)){ 
+    //myTimer.reset(); 
+    myTimer.start();
+    Serial.println("Check1");  
 }
 
-ISR(TIMER0_B) {
+if (myTimer.isReady()) {
+  control_flag = true;
   digitalWrite(5, HIGH);
   signalStart = LOW;
+  //meandr.reset(); 
+  meandr.start();  
+  myTimer.stop();
+  Serial.println("Check2"); 
 }
-  digitalWrite(5, LOW);
-  
 
+if (meandr.isReady()) { 
+  digitalWrite(5, LOW);
+  meandr.stop();
+  Serial.println("Check3"); 
+ }
+
+ 
 }
